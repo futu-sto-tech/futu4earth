1784cebafb8a508523a7f778a9d8d4ea0067e416
Pierre Hedkvist <pierre.hedkvist@futurice.com>
2021-11-05 19:08:38 +0100

    wip

--- App.tsx
+++ App.tsx
@@ -1,27 +1,43 @@
 import * as React from "react";
 
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { colors } from "./colors";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack";
 
-function HomeScreen() {
+type HomeScreenNavigationProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+function HomeScreen({}) {
+  const navigation = useNavigation<HomeScreenNavigationProps>();
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigation.push('Analysis')} title="test">Test</Button>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+function AnalysisScreen({}) {
+  return (
+    <View>Analysis</View>
+  )
+}
+
+type RootStackParamList = {
+  Home: undefined;
+  Analysis: undefined;
+  Report: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Analysis" component={HomeScreen} />
+        <Stack.Screen name="Analysis" component={AnalysisScreen} />
         <Stack.Screen name="Report" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
