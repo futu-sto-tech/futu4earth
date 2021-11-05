b196527c22990e942c798af57ad66a2405edc552
praveen jelish <praveen.jelish@futurice.com>
2021-11-05 18:33:12 +0100

    setup stack navigator and colours constants

---
+++ colors.tsx
@@ -0,0 +1,21 @@
+export const colors = {
+  brand: {
+    primary: "#FFFFFF",
+    secondary: "#009F77",
+    muted: "#CDEDFD",
+    tertiary: "#200A74",
+  },
+  ui: {
+    primary: "#FFFFFF",
+    secondary: "#200A74",
+  },
+  bg: {
+    primary: "#FFFFFF",
+    secondary: "#009F77",
+  },
+  text: {
+    primary: "#FFFFFF",
+    secondary: "#200A74",
+    tertiary: "#FF465A",
+  },
+};
